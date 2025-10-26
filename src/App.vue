46115929887ea7e664d92cd9d1477db4aaf96087
unknown <zyry060910@qq.com>
2025-10-26 20:20:01 +0800

    yy

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  height: 100vh;
+  width: 100vw;
+  align-content: center;
+}
+</style>
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1 @click="handleClick">这是一个test的标题</h1>
+    <p>这是一个段落，<a href="https://www.baidu.com/">这是一个链接</a></p>
+    <ul>
+      <li>列表项 1</li>
+      <li>列表项 2</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const handleClick = () => {
+  alert("点击了哦~");
+};
+</script>
+
+<style scoped>
+.container {
+  justify-items: center;
+}
+
+h1 {
+  cursor: pointer;
+}
+</style>
+```
